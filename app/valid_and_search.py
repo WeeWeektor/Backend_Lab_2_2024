274a1cc7874c0cb24e628de6e5db4d87b3de7046
Viktor <dypluka05@gmail.com>
2024-11-13 17:02:18 +0200

    Add file valid_and_search.py to check the validity of the uuid and whether the record already exists in the database.

---
+++ app/valid_and_search.py
@@ -0,0 +1,26 @@
+import uuid
+
+def is_valid_uuid(identifier):
+    try:
+        uuid_obj = uuid.UUID(str(identifier))
+        return True
+    except ValueError:
+        return False
+
+def search_data_in_db(cur, identifier, table, another_id=None, another_id_2=None, identifier_2=None):
+    if another_id_2 is None and identifier_2 is None:
+        column_name = 'id' if another_id is None else another_id
+        cur.execute(f'''SELECT "{column_name}" FROM "{table}" WHERE "{column_name}" = %s''', (identifier,))
+    else:
+        cur.execute(f'''SELECT "{another_id}", "{another_id_2}" FROM "{table}" WHERE 
+        "{another_id}" = %s AND "{another_id_2}" = %s''', (identifier, identifier_2))
+
+    result = cur.fetchone()
+    cur.close()
+    return result if result else False
+
+def if_currency_in_db(cur, name):
+    cur.execute(f'''SELECT id FROM "Currency" WHERE "currency_name" = %s''', (name,))
+    result = cur.fetchone()
+    cur.close()
+    return result if result else False
