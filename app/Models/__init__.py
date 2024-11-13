c8f10113fca5d14ade2efaf56e681628c5fd9f24
Viktor <dypluka05@gmail.com>
2024-11-13 17:15:18 +0200

    add app\Models\__init__.py

---
+++ app/Models/__init__.py
@@ -0,0 +1,5 @@
+from .BaseModel import BaseModel
+from .CategoryModel import CategoryModel
+from .CurrencyModel import CurrencyModel
+from .UserModel import UserModel
+from .RecordModel import RecordModel
