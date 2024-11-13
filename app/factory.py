5f582f8c41ef2f6414be9f58a2e39b7500b11444
Viktor <dypluka05@gmail.com>
2024-11-13 17:08:13 +0200

    Update file factory.py - added migration

--- app/factory.py
+++ app/factory.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from .DB.db import db
+from flask_migrate import Migrate
+from app.DB.database import init_db
+
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
 
+    app.config.from_pyfile('config.py', silent=True)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     with app.app_context():
+        init_db()
+
+        from app.Models import UserModel, CategoryModel, CurrencyModel, RecordModel
         from app.routes import init_routes
         from app.errors import init_errors
 
