6459b0c86279286c3aff8d4b6879f49eb59cd9c5
Viktor <dypluka05@gmail.com>
2024-12-01 14:36:22 +0200

    Add JWTManager.py file

---
+++ app/JWTManager.py
@@ -0,0 +1,33 @@
+import os
+from flask import jsonify
+
+def JWT_Manager(app, jwt):
+    app.config["JWT_SECRET_KEY"] = os.environ.get('JWT_SECRET_KEY')
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "The token has expired.", "error": "token_expired"}),
+            401,
+        )
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify(
+                {"message": "Signature verification failed.", "error": "invalid_token"}
+            ),
+            401,
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify(
+                {
+                    "description": "Request does not contain an access token.",
+                    "error": "authorization_required",
+                }
+            ),
+            401,
+        )
