d618cb297246bf9fcde149c4eae10a5d52f8268c
Viktor <dypluka05@gmail.com>
2024-12-01 14:35:45 +0200

    Update factory.py

--- app/factory.py
+++ app/factory.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from .DB.db import db
 from flask_migrate import Migrate
 from app.DB.database import init_db
@@ -13,14 +14,18 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
 
+    jwt = JWTManager(app)
+
     with app.app_context():
         init_db()
 
         from app.Models import UserModel, CategoryModel, CurrencyModel, RecordModel
         from app.routes import init_routes
         from app.errors import init_errors
+        from app.JWTManager import JWT_Manager
 
         init_routes(app)
         init_errors(app)
+        JWT_Manager(app, jwt)
 
     return app
